cd644c6972069398b7f35b732cacdeecdc80897d
zhongjiaqi <920093991@qq.com>
2023-06-15 20:22:14 +0800

    新建tag的model方法，处理model回调（暂时没看懂）

--- internal/middleware/translations.go
+++ internal/middleware/translations.go
@@ -12,6 +12,10 @@ import (
 	zh_translations "github.com/go-playground/validator/v10/translations/zh"
 )
 
+// TODO:评论区有人说RegisterDefaultTranslations放在中间件中调用，如果多个用户同时访问，后端容易挂掉
+// 出现 `panic:concurrent map read and map write` 错误，并且无法 recovery
+// 建议在 package 的 init 函数内做初始化
+// 后面再改
 func Translations() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		uni := ut.New(en.New(), zh.New(), zh_Hant_TW.New())
@@ -22,13 +26,10 @@ func Translations() gin.HandlerFunc {
 			switch locale {
 			case "zh":
 				_ = zh_translations.RegisterDefaultTranslations(v, trans)
-				break
 			case "en":
 				_ = en_translations.RegisterDefaultTranslations(v, trans)
-				break
 			default:
 				_ = zh_translations.RegisterDefaultTranslations(v, trans)
-				break
 			}
 			c.Set("trans", trans)
 		}
--- internal/model/tag.go
+++ internal/model/tag.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/go-programming-tour-book/blog-service/pkg/app"
+import (
+	"github.com/go-programming-tour-book/blog-service/pkg/app"
+	"github.com/jinzhu/gorm"
+)
 
 type Tag struct {
 	*Model
@@ -15,4 +18,52 @@ func (t Tag) TableName() string {
 type TagSwagger struct {
 	List  []*Tag
 	Pager *app.Pager
+}
+
+// TODO:*gorm.DB是传参进入的？
+func (t Tag) Count(db *gorm.DB) (int, error) {
+	var count int
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+
+	// Model:指定运行 DB 操作的模型实例，默认解析该结构体的名字为表名，格式为大写驼峰转小写下划线驼峰。若情况特殊，也可以编写该结构体的 TableName 方法用于指定其对应返回的表名。
+	if err := db.Model(&t).Where("is_del = ?", 0).Count(&count).Error; err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
+	var tags []*Tag
+	var err error
+
+	if pageOffset >= 0 && pageSize > 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+
+	db = db.Where("state = ?", t.State)
+
+	if err = db.Model(&t).Where("is_del = ?", 0).Find(&tags).Error; err != nil {
+		return nil, err
+	}
+	return tags, nil
+}
+
+func (t Tag) Create(db *gorm.DB) error {
+	return db.Create(&t).Error
+}
+
+func (t Tag) Update(db *gorm.DB) error {
+	return db.Model(&Tag{}).Where("id = ? AND is_del = ?", t.ID, 0).Update(t).Error
+}
+
+func (t Tag) Delete(db *gorm.DB) error {
+	return db.Where("id = ? AND is_del = ?", t.Model.ID, 0).Delete(&t).Error
 }
